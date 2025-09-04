3e85917aadfee41eff34a12c220838e91d43b4a5
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-09-04 15:09:44 +0200

    Version bump to 1.0.4

--- src/js/classic/DocPanel.js
+++ src/js/classic/DocPanel.js
@@ -4,7 +4,7 @@ Ext.define('Tualo.binary.docx.views.DocPanel', {
     items: [
         {
             xtype: 'tualo_binary_docxiframe',
-            src: 'https://fb-wvd.tualo.io/server/binary-docx/open/176464',
+            src: 'https://fb-wvd.tualo.io/server/binary-docx/open/6496',
         }
     ]
 });
